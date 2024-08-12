36fab155238789769947bd847b995a46cb8f498e
Facundo Andrean <76758462+facuandrean@users.noreply.github.com>
2024-08-12 20:43:33 -0300

    Add files via upload

---
+++ backend/app/middlewares/auth.js
@@ -0,0 +1,29 @@
+import jwt from 'jsonwebtoken';
+import { config } from "../config/config.js";
+
+const verifyToken = (req, res, next) => {
+
+    // Obtenemos el token desde las cookies.
+    const token = req.cookies.access_token;
+
+    if (!token) {
+        return res.status(401).json({
+            message: 'Acceso denegado. Token no proporcionado.'
+        });
+    };
+    
+    jwt.verify(token, config.secretJwtKey, (err, user) => {
+        if (err) {
+            console.log('err', err);
+            return res.status(401).json({
+                message: 'Acceso denegado. Token inválido.'
+            });
+        }
+        req.user = user;
+        next();
+    });
+    
+};
+
+
+export { verifyToken };
