f9de18ac13d44bb3703436d13b551184db2bfe8e
Facundo Andrean <76758462+facuandrean@users.noreply.github.com>
2024-08-13 11:21:49 -0300

    Add files via upload

---
+++ frontend/src/components/auth.js
@@ -0,0 +1,29 @@
+import renderIndexAdmin from "./indexAdminView.js";
+import renderIndexEmployee from "./indexEmployeeView.js";
+
+export default async function checkAuthentication() {
+
+    try {
+        
+        const response = await fetch('https://peluqueria-invasion.vercel.app/verify-token', { credentials: 'include' });
+
+        if (!response.ok || response.status === 401) {
+            window.location.href = '/login.html';
+        } else {
+
+            // Leemos el cuerpo de la respuesta, en el cual recibimos el usuario logueado.
+            const data = await response.json(); 
+
+            if (data.user.Rol === "Empleado") {
+                renderIndexEmployee(data);
+            } else if (data.user.Rol === "Admin") {
+                renderIndexAdmin(data);
+            }
+            
+        } 
+
+    } catch (error) {
+        window.location.href = '/login.html';
+    };
+
+};
---
+++ frontend/src/components/login.js
@@ -0,0 +1,28 @@
+const mensajeError = document.getElementsByClassName("error")[0];
+
+document.getElementById('login-form').addEventListener('submit', async (event) => {
+    
+    event.preventDefault();
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    const response = await fetch('https://peluqueria-invasion.vercel.app/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json', 
+        },
+        body: JSON.stringify({ Nombre: username, Contrasena: password }),
+        credentials: 'include',
+    });
+    
+    if (response.ok) {
+        window.location.href = '/';
+    } 
+    
+    if (!response.ok) {
+        return mensajeError.classList.toggle("escondido", false);
+    }
+
+});
+  
