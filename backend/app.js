cfa4ab508450a7062c58b445364de222692bfb05
Facundo Andrean <76758462+facuandrean@users.noreply.github.com>
2024-08-13 12:04:34 -0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ const app = express();
 // Middlewares.
 app.use(express.json());
 app.use(cors({
-    origin: "https://peluqueria-invasion-front.vercel.app",
+    origin: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true
 }));
