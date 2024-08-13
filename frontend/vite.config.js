e8e0ccbe7f4abf650250caacdf68dd3cb079123e
Facundo Andrean <76758462+facuandrean@users.noreply.github.com>
2024-08-13 05:52:42 -0300

    Add files via upload

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,26 +1,39 @@
+// import { defineConfig } from 'vite';
+// import { createHtmlPlugin } from 'vite-plugin-html';
+
+// export default defineConfig({
+//   build: {
+//     outDir: 'dist', // Directorio de salida
+//     rollupOptions: {
+//       input: {
+//         main: 'index.html',
+//         login: 'login.html', // Añade cualquier otro archivo HTML aquí
+//       },
+//     },
+//   },
+//   plugins: [
+//     createHtmlPlugin({
+//       minify: true,
+//     }),
+//   ],
+//   resolve: {
+//     alias: {
+//       // Añade alias si necesitas simplificar rutas
+//       '@': '/src',
+//     },
+//   },
+// });
+
 import { defineConfig } from 'vite';
-import { createHtmlPlugin } from 'vite-plugin-html';
+import { resolve } from 'path';
 
 export default defineConfig({
   build: {
-    outDir: 'dist', // Directorio de salida
     rollupOptions: {
       input: {
-        main: 'index.html',
-        login: 'login.html', // Añade cualquier otro archivo HTML aquí
-      },
-    },
-  },
-  plugins: [
-    createHtmlPlugin({
-      minify: true,
-    }),
-  ],
-  resolve: {
-    alias: {
-      // Añade alias si necesitas simplificar rutas
-      '@': '/src',
-    },
-  },
+        main: resolve(__dirname, 'index.html'),
+        login: resolve(__dirname, 'login.html')
+      }
+    }
+  }
 });
-
